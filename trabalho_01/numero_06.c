53a5b7e4c111ea6e379a22e8e635950749553775
VictorHugo <mgvitinhon1@gmail.com>
2025-05-05 19:52:05 -0300

    Update numero_06.c
    corrigido auto-increment das tentativas e remoção da linha que atrapalhava incrementar, adicionada a a alteração da aleatoriedade constante através do srand(time(NULL)) onde toda vez que executar o programa ele vai alterar o número secreto.

--- trabalho_01/numero_06.c
+++ trabalho_01/numero_06.c
@@ -5,8 +5,9 @@
 int main() {
    int num ,tentativaNum, tentativas = 0;
    
+   
+    srand(time(NULL));
    	num =(rand()% 100)+1;
-   	printf("o numero secreto é %d\n",num);
    	printf("____________________________________\n");
    	printf("bem vindo ao jogo do numero Secreto\n");
    	
@@ -14,21 +15,22 @@ int main() {
 	do{
 	printf("Digite um numero de 1 a 100 :");
    	scanf("%d",&tentativaNum);
-   	tentativas= tentativas ++;
+   	tentativas= tentativas++;
    
   
   	
    	if (tentativaNum == num ){
    		printf("Parabéns!\nVocê acertou o número %d\nCom um total de %d tentativas!\n", num, tentativas);
 	   }
+	  
 		   else if(tentativaNum <num)
 		   {
-		   	printf("o numero secreto é maior ");
+		   	printf("o numero secreto é maior \n ");
 		   }
 		   	else{
-		   		printf("o numero secreto é menor");
+		   		printf("o numero secreto é menor \n ");
 			   }
-			   tentativas= tentativas + 1;
+			   
 	}while(tentativaNum != num);
     return 0;
     
