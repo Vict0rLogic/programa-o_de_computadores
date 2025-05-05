31cfcfb689f11c723ea29a81f12312639db4a941
VictorHugo <mgvitinhon1@gmail.com>
2025-05-05 12:06:31 -0300

    Create numero_06.c
    jogo do numero secreto

---
+++ trabalho_01/numero_06.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int num ,tentativaNum, tentativas = 0;
+   
+   	num =(rand()% 100)+1;
+   	printf("o numero secreto é %d\n",num);
+   	printf("____________________________________\n");
+   	printf("bem vindo ao jogo do numero Secreto\n");
+   	printf("Digite um numero de 1 a 100 :");
+   	scanf("%d",&tentativaNum);
+   
+   
+   do{
+  	
+   	if (tentativaNum == num ){
+   		printf("Parabéns!\nVocê acertou o número %d\nCom um total de %d tentativas!\n", num, tentativas);
+	   }
+		   else if(tentativaNum <num)
+		   {
+		   	printf("o numero secreto é maior ");
+		   }
+		   	else{
+		   		printf("o numero secreto é menor");
+			   }
+			   tentativas= tentativas + 1;
+	}while(tentativaNum != num)
+    return 0;
+    
+}
